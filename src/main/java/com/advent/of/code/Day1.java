8bb726557a2033fd5f5cdc4c0b3b98a36cefde82
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-08-05 07:20:56 -0400

    a

---
+++ src/main/java/com/advent/of/code/Day1.java
@@ -0,0 +1,5 @@
+package com.advent.of.code;
+
+public class Day1 {
+  public static int Add(int a, int b) { return a + b; }
+}
---
+++ src/test/java/com/advent/of/code/Day1Tests.java
@@ -0,0 +1,12 @@
+
+package com.advent.of.code;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Day1Tests {
+  @Test
+  public void testAdd() {
+    assertEquals(3, Day1.Add(1, 2));
+  }
+}
