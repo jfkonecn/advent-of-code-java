b32b823c9483d2ee91a26459d837e7e479e51c5a
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-08-05 08:47:10 -0400

    a

--- src/main/java/com/advent/of/code/day1/Day1.java
+++ src/main/java/com/advent/of/code/day1/Day1.java
@@ -1,9 +1,38 @@
 package com.advent.of.code.day1;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day1 {
   public static int Part1(List<String> input) {
-    return input.stream().mapToInt(Integer::parseInt).sum();
+    List<Integer> numbers = input.stream()
+                                .mapToInt(Integer::parseInt)
+                                .boxed()
+                                .collect(Collectors.toList());
+    for (Integer outter : numbers) {
+      for (Integer inner : numbers) {
+        if (outter != inner && outter + inner == 2020) {
+          return outter * inner;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public static int Part2(List<String> input) {
+    List<Integer> numbers = input.stream()
+                                .mapToInt(Integer::parseInt)
+                                .boxed()
+                                .collect(Collectors.toList());
+    for (Integer first : numbers) {
+      for (Integer second : numbers) {
+        for (Integer third : numbers) {
+          if (first + second + third == 2020) {
+            return first * second * third;
+          }
+        }
+      }
+    }
+    return -1;
   }
 }
--- src/test/java/com/advent/of/code/day1/Day1Tests.java
+++ src/test/java/com/advent/of/code/day1/Day1Tests.java
@@ -1,5 +1,6 @@
 package com.advent.of.code.day1;
 import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -15,7 +16,19 @@ public class Day1Tests {
 
   @Test
   public void real1() throws IOException {
+    List<String> lines = getRealText("day1");
+    assertEquals(252724, Day1.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
     List<String> lines = getExampleText("day1");
-    assertEquals(-1, Day1.Part1(lines));
+    assertEquals(241861950, Day1.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("day1");
+    assertEquals(276912720, Day1.Part2(lines));
   }
 }
