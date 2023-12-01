c33d661542c8dabe8ea60ac8b6694bba2c176401
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-01 06:55:55 -0500

    a

---
+++ src/test/java/com/advent/of/code/year2023/day01/Day01Tests.java
@@ -0,0 +1,34 @@
+package com.advent.of.code.year2023.day01;
+import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class Day01Tests {
+  @Test
+  public void example1() throws IOException {
+    List<String> lines = getExampleText("year2023", "day01");
+    assertEquals(514579, Day01.Part1(lines));
+  }
+
+  @Test
+  public void real1() throws IOException {
+    List<String> lines = getRealText("year2023", "day01");
+    assertEquals(252724, Day01.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
+    List<String> lines = getExampleText("year2023", "day01");
+    assertEquals(241861950, Day01.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("year2023", "day01");
+    assertEquals(276912720, Day01.Part2(lines));
+  }
+}
