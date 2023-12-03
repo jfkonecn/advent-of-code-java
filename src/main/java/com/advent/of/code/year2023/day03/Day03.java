de9101ce3aa0d5b132fad57e67a2c16c6bed7946
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-03 08:01:24 -0500

    a

---
+++ src/main/java/com/advent/of/code/year2023/day03/Day03.java
@@ -0,0 +1,54 @@
+package com.advent.of.code.year2023.day03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day03 {
+  private record GameSet(int red, int green, int blue) {}
+
+  private record Game(int id, ArrayList<GameSet> gameSets) {}
+
+  private static Game parseGame(String input) {
+    var gameSets = new ArrayList<GameSet>();
+    var id = Integer.parseInt(input.substring(input.indexOf(" ") + 1, input.indexOf(":")));
+    var gameSetsStr = input.substring(input.indexOf(":") + 2);
+    for (var gameSetString : gameSetsStr.split("; ")) {
+      var red = 0;
+      var green = 0;
+      var blue = 0;
+      for (var colorStr : gameSetString.split(", ")) {
+        var splitColor = colorStr.split(" ");
+        var total = Integer.parseInt(splitColor[0]);
+        var colorName = splitColor[1];
+
+        if (colorName.equals("red")) {
+          red = total;
+        } else if (colorName.equals("green")) {
+          green = total;
+        } else if (colorName.equals("blue")) {
+          blue = total;
+        }
+      }
+      gameSets.add(new GameSet(red, green, blue));
+    }
+    return new Game(id, gameSets);
+  }
+
+  public static int Part1(List<String> input) {
+    return input.stream()
+        .map(x -> parseGame(x))
+        .filter(
+            x -> {
+              var totalRed = x.gameSets().stream().mapToInt(GameSet::red).max().getAsInt();
+              var totalGreen = x.gameSets().stream().mapToInt(GameSet::green).max().getAsInt();
+              var totalBlue = x.gameSets().stream().mapToInt(GameSet::blue).max().getAsInt();
+              return totalRed <= 12 && totalGreen <= 13 && totalBlue <= 14;
+            })
+        .mapToInt(Game::id)
+        .sum();
+  }
+
+  public static int Part2(List<String> input) {
+    return 0;
+  }
+}
---
+++ src/test/java/com/advent/of/code/year2023/day03/Day03Tests.java
@@ -0,0 +1,35 @@
+package com.advent.of.code.year2023.day03;
+
+import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class Day03Tests {
+  @Test
+  public void example1() throws IOException {
+    List<String> lines = getExampleText("year2023", "day02");
+    assertEquals(4361, Day03.Part1(lines));
+  }
+
+  @Test
+  public void real1() throws IOException {
+    List<String> lines = getRealText("year2023", "day02");
+    assertEquals(0, Day03.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
+    List<String> lines = getExampleText("year2023", "day02");
+    assertEquals(0, Day03.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("year2023", "day02");
+    assertEquals(0, Day03.Part2(lines));
+  }
+}
