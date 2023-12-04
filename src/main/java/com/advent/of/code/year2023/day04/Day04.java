e1902c42d49d1886d14685c010502568a2867363
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-04 07:47:43 -0500

    a

---
+++ src/main/java/com/advent/of/code/year2023/day04/Day04.java
@@ -0,0 +1,43 @@
+package com.advent.of.code.year2023.day04;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day04 {
+
+  private record Card(int id, List<Integer> winningNumbers, List<Integer> myNumbers) {}
+
+  private static Card parseCard(String input) {
+    var headNumbersSplit = input.split(":");
+    var id = Integer.parseInt(headNumbersSplit[0].replace("Card ", "").trim());
+    var numbersSplit = headNumbersSplit[1].trim().split("\\|");
+    var winningNumbers =
+        List.of(numbersSplit[0].split(" ")).stream()
+            .filter(x -> !x.isBlank())
+            .map(x -> Integer.parseInt(x.trim()))
+            .collect(Collectors.toList());
+    var myNumbers =
+        List.of(numbersSplit[1].split(" ")).stream()
+            .filter(x -> !x.isBlank())
+            .map(x -> Integer.parseInt(x.trim()))
+            .collect(Collectors.toList());
+    return new Card(id, winningNumbers, myNumbers);
+  }
+
+  public static int Part1(List<String> input) {
+    var temp =
+        input.stream()
+            .map(Day04::parseCard)
+            .filter(
+                x -> {
+                  return true;
+                })
+            .collect(Collectors.toList());
+
+    return 0;
+  }
+
+  public static int Part2(List<String> input) {
+    return 0;
+  }
+}
---
+++ src/test/java/com/advent/of/code/year2023/day04/Day04Tests.java
@@ -0,0 +1,35 @@
+package com.advent.of.code.year2023.day04;
+
+import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class Day04Tests {
+  @Test
+  public void example1() throws IOException {
+    List<String> lines = getExampleText("year2023", "day04");
+    assertEquals(13, Day04.Part1(lines));
+  }
+
+  @Test
+  public void real1() throws IOException {
+    List<String> lines = getRealText("year2023", "day04");
+    assertEquals(0, Day04.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
+    List<String> lines = getExampleText("year2023", "day04");
+    assertEquals(0, Day04.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("year2023", "day04");
+    assertEquals(0, Day04.Part2(lines));
+  }
+}
