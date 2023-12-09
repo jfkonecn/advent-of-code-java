b1b799b348eb8ee79cbb96a1e054837a897aeb5e
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-09 09:24:54 -0500

    a

---
+++ src/main/java/com/advent/of/code/year2023/day07/Day07.java
@@ -0,0 +1,13 @@
+package com.advent.of.code.year2023.day07;
+
+import java.util.List;
+
+public class Day07 {
+  public static int Part1(List<String> input) {
+    return -1;
+  }
+
+  public static int Part2(List<String> input) {
+    return -1;
+  }
+}
---
+++ src/test/java/com/advent/of/code/year2023/day07/Day07Tests.java
@@ -0,0 +1,35 @@
+package com.advent.of.code.year2023.day07;
+
+import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class Day07Tests {
+  @Test
+  public void example1() throws IOException {
+    List<String> lines = getExampleText("year2023", "day07");
+    assertEquals(0, Day07.Part1(lines));
+  }
+
+  @Test
+  public void real1() throws IOException {
+    List<String> lines = getRealText("year2023", "day07");
+    assertEquals(0, Day07.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
+    List<String> lines = getExampleText("year2023", "day07");
+    assertEquals(0, Day07.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("year2023", "day07");
+    assertEquals(0, Day07.Part2(lines));
+  }
+}
