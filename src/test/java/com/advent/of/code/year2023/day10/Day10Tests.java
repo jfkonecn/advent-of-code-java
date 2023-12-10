de75089065d00673f282dcb0d4d1705ddbb89e43
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-12-10 11:47:43 -0500

    a

---
+++ src/test/java/com/advent/of/code/year2023/day10/Day10Tests.java
@@ -0,0 +1,42 @@
+package com.advent.of.code.year2023.day10;
+
+import static com.advent.of.code.Helpers.getExample2Text;
+import static com.advent.of.code.Helpers.getExampleText;
+import static com.advent.of.code.Helpers.getRealText;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+import org.junit.Test;
+
+public class Day10Tests {
+  @Test
+  public void example1a() throws IOException {
+    List<String> lines = getExampleText("year2023", "day10");
+    assertEquals(4, Day10.Part1(lines));
+  }
+
+  @Test
+  public void example1b() throws IOException {
+    List<String> lines = getExample2Text("year2023", "day10");
+    assertEquals(8, Day10.Part1(lines));
+  }
+
+  @Test
+  public void real1() throws IOException {
+    List<String> lines = getRealText("year2023", "day10");
+    assertEquals(0, Day10.Part1(lines));
+  }
+
+  @Test
+  public void example2() throws IOException {
+    List<String> lines = getExampleText("year2023", "day10");
+    assertEquals(0, Day10.Part2(lines));
+  }
+
+  @Test
+  public void real2() throws IOException {
+    List<String> lines = getRealText("year2023", "day10");
+    assertEquals(0, Day10.Part2(lines));
+  }
+}
